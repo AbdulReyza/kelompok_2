a5f24b0a9a1f94e4b2b0a92db2a8ed62339aa536
RIOKOWI <achyarrio2004@gmail.com>
2025-12-20 09:45:47 +0700

    feat: add gradient color and university campus

--- lib/presentation/pages/splash_global.dart
+++ lib/presentation/pages/splash_global.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SplashGlobal extends StatefulWidget {
+  static const routeName = '/splashGlobal';
   const SplashGlobal({super.key});
 
   @override
@@ -10,14 +11,39 @@ class SplashGlobal extends StatefulWidget {
 class _SplashGlobalState extends State<SplashGlobal> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
+    return Container(
+        decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFFeb1e34),
+            Colors.white,
+          ],
+        ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-            children: [],
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          // title: const Text("Dashboard"),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          scrolledUnderElevation: 0,
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      image: 
+                      DecorationImage(
+                        image: AssetImage("assets/images/logo_global.jpeg")
+                      )
+                    ),
+                  ),
+          ),
         ),
       ),
     );
