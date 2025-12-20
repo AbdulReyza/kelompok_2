998d40fa1df466f6e46763743541446aa9fba898
AbdulReyza <abdulreyza6@gmail.com>
2025-12-20 23:25:41 +0700

    first commit

---
+++ lib/presentation/pages/splash_reyza.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashReyza extends StatelessWidget {
+  const SplashReyza({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
