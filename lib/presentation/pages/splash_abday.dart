6ed362fb5e9f0ee77cc2470ef72e9989ca804cfb
abday_hafidz <abday.hafidz23@gmail.com>
2025-12-21 20:28:56 +0700

    Implement SplashAbday screen

--- lib/presentation/pages/splash_abday.dart
+++ lib/presentation/pages/splash_abday.dart
@@ -5,7 +5,7 @@ class SplashAbday extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Text('Splash Abday'),
       ),
