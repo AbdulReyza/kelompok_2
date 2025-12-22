319f9e7557c6e8368b0bda8d703e589fe890acaa
abday_hafidz <abday.hafidz23@gmail.com>
2025-12-22 16:57:03 +0700

    feat(profile): membuat isi didalam page profile mahasiswa

--- lib/presentation/profile/abday_profile.dart
+++ lib/presentation/profile/abday_profile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart'; // import Lottie
+import 'package:lottie/lottie.dart'; 
 
 class ProfileAbday extends StatelessWidget {
   static const routeName = '/profileAbday';
@@ -8,22 +8,42 @@ class ProfileAbday extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Scaffold jadi root dari halaman profile
-      backgroundColor: const Color(0xFF12130F), // warna background
+      backgroundColor: const Color(0xFF12130F),
       appBar: AppBar(
         title: const Text("Profil Mahasiswa"),
         backgroundColor: const Color(0xFF5B9279),
         elevation: 0,
       ),
       body: SingleChildScrollView(
-         padding: const EdgeInsets.all(20), // jarak dari tepi layar
+         padding: const EdgeInsets.all(20), 
   child: Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      // semua elemen konten nantinya ditaruh di sini
-    ],
-  ),
-      ), // nanti akan kita isi di step berikut
+  crossAxisAlignment: CrossAxisAlignment.center,
+  children: [
+    CircleAvatar(
+      radius: 80,
+      backgroundImage: AssetImage('assets/images/abdayprofile.jpg'),
+    ),
+    SizedBox(height: 15),
+    Lottie.asset(
+      'assets/animations/profile_student.json',
+      width: 150,
+      height: 150,
+      repeat: true,
+    ),
+    Text(
+      "Muhammad abday abdul hafidz",
+      style: TextStyle(
+        fontFamily: 'Pixel',
+        fontWeight: FontWeight.bold,
+        fontSize: 26,
+        color: Color(0xFFEAE6E5),
+      ),
+    ),
+    // 
+  ],
+),
+
+      ), 
     );
   }
 }
