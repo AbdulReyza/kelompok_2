1fd91ac054b77b3e6025c57524ddc47d5ac1b63d
RIOKOWI <achyarrio2004@gmail.com>
2025-12-24 05:15:57 +0700

    feat: fix routing and gesture detector ini profile

--- lib/presentation/profiles/abday_profile.dart
+++ lib/presentation/profiles/abday_profile.dart
@@ -12,82 +12,80 @@ class ProfileAbday extends StatefulWidget {
 class _ProfileAbdayState extends State<ProfileAbday> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF12130F),
-      appBar: AppBar(
-        title: const Text(
-          "PROFIL MAHASISWA",
-          style: TextStyle(fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: (){
+        Navigator.pushNamed(
+          context,
+          ProfileReyza.routeName
+        );
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xFF12130F),
+        appBar: AppBar(
+          title: const Text(
+            "PROFIL ABDAY",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: const Color(0xFFCCFB0F),
+          elevation: 0,
+          centerTitle: true,
         ),
-        backgroundColor: const Color(0xFFCCFB0F),
-        elevation: 0,
-        centerTitle: true,
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(20),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ProfileReyza(),
+        body: Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      CircleAvatar(
+                        radius: 80,
+                        backgroundImage:
+                            AssetImage('assets/images/abdayprofile.jpg'),
+                      ),
+                      SizedBox(height: 15),
+                      Text(
+                        "Muhammad Abday Abdul Hafidz",
+                        style: TextStyle(
+                          fontFamily: 'Pixel',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26,
+                          color: Color(0xFFEAE6E5),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "NIM: 1123150093 | KELAS: TI-23-SE-1",
+                        style: TextStyle(
+                          fontFamily: 'Pixel',
+                          fontSize: 16,
+                          color: Color(0xFFCCFB0F),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 15),
+                      Text(
+                        "Keahlian:",
+                        style: TextStyle(
+                          fontFamily: 'Pixel',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Color(0xFF473BFD),
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "- Flutter Basic\n- JavaScript\n- HTML & CSS",
+                        style: TextStyle(
+                          fontFamily: 'Pixel',
+                          fontSize: 16,
+                          color: Color(0xFFEAE6E5),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
                   ),
-                );
-              },
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                  CircleAvatar(
-                    radius: 80,
-                    backgroundImage:
-                        AssetImage('assets/images/abdayprofile.jpg'),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    "Muhammad Abday Abdul Hafidz",
-                    style: TextStyle(
-                      fontFamily: 'Pixel',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 26,
-                      color: Color(0xFFEAE6E5),
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "NIM: 1123150093 | KELAS: TI-23-SE-1",
-                    style: TextStyle(
-                      fontFamily: 'Pixel',
-                      fontSize: 16,
-                      color: Color(0xFFCCFB0F),
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    "Keahlian:",
-                    style: TextStyle(
-                      fontFamily: 'Pixel',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Color(0xFF473BFD),
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    "- Flutter Basic\n- JavaScript\n- HTML & CSS",
-                    style: TextStyle(
-                      fontFamily: 'Pixel',
-                      fontSize: 16,
-                      color: Color(0xFFEAE6E5),
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
             ),
-        ),
+          ),
       ),
     );
   }
