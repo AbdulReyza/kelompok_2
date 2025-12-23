806ab34c7ca4f7137165ce6d04bff0766c8496c3
RIOKOWI <achyarrio2004@gmail.com>
2025-12-24 04:54:36 +0700

    feat: membuat button logout

--- lib/presentation/pages/setting.dart
+++ lib/presentation/pages/setting.dart
@@ -12,13 +12,34 @@ class _SettingPageState extends State<SettingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Setting"),
+        title: Text("Setting"),
         actions: const [],
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-            children: [],
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+              children: [
+                Container(
+                    child: SizedBox(
+                      height: 40,
+                      width: 100,
+                      child: ElevatedButton(
+                        onPressed: (){
+                        }, 
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                        ),
+                        child: Text('LOGOUT',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),),
+                        
+                      ),
+                    ),
+                  ),
+              ],
+          ),
         ),
       ),
     );
