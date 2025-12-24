bbbfea48af1210158acc5eebb2a33a5750e08533
RIOKOWI <achyarrio2004@gmail.com>
2025-12-24 20:16:58 +0700

    feat: membuat data model reyza

--- lib/data/models/developers.dart
+++ lib/data/models/developers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kelompok_2/presentation/profiles/reyza_profile.dart';
 
 class DevelopersModel {
   String name;
@@ -13,4 +14,16 @@ class DevelopersModel {
     required this.iconPath,
   });
 
+  static List<DevelopersModel> getDevelopers() {
+    List<DevelopersModel> developers = [];
+
+    developers.add(
+      DevelopersModel(
+        name: 'Abdul Reyza Ramadhanu', 
+        boxColor: Color(0xFFCCFB0F), 
+        route: ProfileReyza.routeName, 
+        iconPath: 'assets/images/reyza.jpg'
+      ),
+    );
+  }
 }
