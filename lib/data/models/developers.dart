57cbd7d60cb54d2d5e65ff786e26f43b45bead91
RIOKOWI <achyarrio2004@gmail.com>
2025-12-24 20:12:54 +0700

    feat: membuat class developers model

---
+++ lib/data/models/developers.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DevelopersModel {
+  String name;
+  Color boxColor;
+  String route;
+  String iconPath;
+
+  DevelopersModel({
+    required this.name,
+    required this.boxColor,
+    required this.route,
+    required this.iconPath,
+  });
+
+}
