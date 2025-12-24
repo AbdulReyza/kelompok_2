e1daa5caa9497ba1f26f95680504622d804f345d
RIOKOWI <achyarrio2004@gmail.com>
2025-12-24 20:01:37 +0700

    feat: membuat listview

--- lib/presentation/pages/about.dart
+++ lib/presentation/pages/about.dart
@@ -11,16 +11,45 @@ class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("About"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-            children: [],
+      appBar: AppBar(),
+      body: Center(
+        child: ListView.separated(
+          itemCount: 4,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 60,
+              color: Colors.blue[100],
+              child: Center(child: Text('Item ${index + 1}', style: TextStyle(fontSize: 18))),
+            
+            );
+          }, 
+          separatorBuilder: (context, index) {
+            return Divider(
+              color: Colors.grey, thickness: 1,
+            );
+          }, 
         ),
       ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text("OUR DEVELOPERS",
+        style: TextStyle(
+          color: Color(0xFFCCFB0F),
+        ),
+      ),
+      centerTitle: true,
+      actions: const [],
     );
   }
 }
