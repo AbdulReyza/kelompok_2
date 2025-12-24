48943454e9918c377ee5ed3b36ee463981e71320
RIOKOWI <achyarrio2004@gmail.com>
2025-12-25 06:34:03 +0700

    style: add padding

--- lib/presentation/pages/about.dart
+++ lib/presentation/pages/about.dart
@@ -27,60 +27,76 @@ class _AboutPageState extends State<AboutPage> {
       backgroundColor: Color(0xFF12130F),
       appBar: myAppBar(),
       body: Center(
-          child: ListView.separated(
-            itemCount: developers.length,
-            scrollDirection: Axis.vertical,
-            itemBuilder: (context, index) {
-              
-              return InkWell(
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    developers[index].route
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 15, right: 15),
-                  child: Container(
-                    height: 150,
-                    decoration: BoxDecoration(
-                      color: developers[index].boxColor,
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 40,
-                            backgroundImage: AssetImage(developers[index].iconPath),
-                          ),
-                          SizedBox(width: 20,),
-                          Expanded(
-                            child: Text(
-                              textAlign: TextAlign.center,
-                              developers[index].name, 
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                                color: Colors.white,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  
-                  ),
-                ),
-              );
-            }, 
-            separatorBuilder: (context, index) {
-              return SizedBox(height: 20);
-            }, 
-          ),
+          child: myDev(),
       ),
     );
+  }
+
+  ListView myDev() {
+    return ListView.separated(
+          padding:  EdgeInsets.only(
+            top: 50, 
+            bottom: 70
+          ),
+          itemCount: developers.length,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) {
+            
+            return InkWell(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  developers[index].route
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: developers[index].boxColor,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.white.withOpacity(0.3),
+                        blurRadius: 12,
+                        spreadRadius: 2,
+                        offset: Offset(0, 6),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 40,
+                          backgroundImage: AssetImage(developers[index].iconPath),
+                        ),
+                        SizedBox(width: 20,),
+                        Expanded(
+                          child: Text(
+                            textAlign: TextAlign.center,
+                            developers[index].name, 
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                
+                ),
+              ),
+            );
+          }, 
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 20);
+          }, 
+        );
   }
 }
 
