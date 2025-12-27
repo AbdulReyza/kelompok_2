ccbd4de77810a6d60fa2af4d7c005758b00ef128
RIOKOWI <achyarrio2004@gmail.com>
2025-12-27 11:39:55 +0700

    style: rapihin code

--- lib/presentation/pages/setting.dart
+++ lib/presentation/pages/setting.dart
@@ -23,63 +23,69 @@ class _SettingPageState extends State<SettingPage> {
         child: SingleChildScrollView(
           padding: EdgeInsets.all(10.0),
           child: Column(
-              children: [
-                ClipOval(
-  child: BackdropFilter(
-    filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-    child: Container(
-      width: 56,
-      height: 56,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.white.withOpacity(0.15),
-        border: Border.all(
-          color: Colors.white.withOpacity(0.25),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.25),
-            blurRadius: 10,
-            offset: Offset(0, 6),
-          ),
-        ],
-      ),
-      child: IconButton(
-        icon: const Icon(
-          Icons.power_settings_new,
-          color: Colors.grey,
-        ),
-        onPressed: () {
-          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: const Text("Logout"),
-                              content: const Text("Apakah Anda yakin ingin keluar?"),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.pop(context),
-                                  child: const Text("Batal"),
-                                ),
-                                TextButton(
-                                  onPressed: () async {
-                                    await auth.signOut();
-                                    if (context.mounted) {
-                                      Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false);
-                                    }
-                                  },
-                                  child: const Text("Ya, Keluar", style: TextStyle(color: Colors.red)),
-                                ),
-                              ],
+            children: [
+              ClipOval(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+                  child: Container(
+                    width: 56,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white.withOpacity(0.15),
+                      border: Border.all(color: Colors.white.withOpacity(0.25)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.25),
+                          blurRadius: 10,
+                          offset: Offset(0, 6),
+                        ),
+                      ],
+                    ),
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.power_settings_new,
+                        color: Colors.grey,
+                      ),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text("Logout"),
+                            content: const Text(
+                              "Apakah Anda yakin ingin keluar?",
                             ),
-                          );
-        },
-      ),
-    ),
-  ),
-)
-
-
-              ],
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: const Text("Batal"),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  await auth.signOut();
+                                  if (context.mounted) {
+                                    Navigator.of(
+                                      context,
+                                    ).pushNamedAndRemoveUntil(
+                                      '/login',
+                                      (route) => false,
+                                    );
+                                  }
+                                },
+                                child: const Text(
+                                  "Ya, Keluar",
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -87,37 +93,35 @@ class _SettingPageState extends State<SettingPage> {
   }
 
   PreferredSizeWidget glassAppBar() {
-  return PreferredSize(
-    preferredSize: const Size.fromHeight(kToolbarHeight),
-    child: ClipRect(
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 18, sigmaY: 18),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.15),
-            border: Border(
-              bottom: BorderSide(
-                color: Colors.white.withOpacity(0.2),
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(kToolbarHeight),
+      child: ClipRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 18, sigmaY: 18),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.15),
+              border: Border(
+                bottom: BorderSide(color: Colors.white.withOpacity(0.2)),
               ),
+              borderRadius: BorderRadius.circular(20),
             ),
-            borderRadius: BorderRadius.circular(20)
-          ),
-          child: AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            centerTitle: true,
-            title: const Text(
-              "SETTING",
-              style: TextStyle(
-                color: Color(0xFFEAE6E5),
-                fontWeight: FontWeight.bold,
+            child: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+              title: const Text(
+                "SETTING",
+                style: TextStyle(
+                  color: Color(0xFFEAE6E5),
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
