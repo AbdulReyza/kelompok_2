96ce36e34b6503a0a6ec818bbbbca1d6bc4aaa1c
abday_hafidz <abday.hafidz23@gmail.com>
2025-12-28 19:44:41 +0700

    feat:nambahin card  profile abday page

--- lib/presentation/profile/abday_profile.dart
+++ lib/presentation/profile/abday_profile.dart
@@ -24,71 +24,73 @@ class _ProfileAbdayState extends State<ProfileAbday> {
         centerTitle: true,
       ),
       body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(20),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ProfileReyza(),
-                  ),
-                );
-              },
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                  CircleAvatar(
-                    radius: 80,
-                    backgroundImage:
-                        AssetImage('assets/images/abdayprofile.jpg'),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    "Muhammad Abday Abdul Hafidz",
-                    style: TextStyle(
-                      fontFamily: 'Pixel',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 26,
-                      color: Color(0xFFEAE6E5),
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    "NIM: 1123150093 | KELAS: TI-23-SE-1",
-                    style: TextStyle(
-                      fontFamily: 'Pixel',
-                      fontSize: 16,
-                      color: Color(0xFFCCFB0F),
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    "Keahlian:",
-                    style: TextStyle(
-                      fontFamily: 'Pixel',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Color(0xFF473BFD),
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    "- Flutter Basic\n- JavaScript\n- HTML & CSS",
-                    style: TextStyle(
-                      fontFamily: 'Pixel',
-                      fontSize: 16,
-                      color: Color(0xFFEAE6E5),
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
+  child: SingleChildScrollView(
+    padding: const EdgeInsets.all(20),
+    child: Card(
+      color: const Color(0xFF5B9279),
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            CircleAvatar(
+              radius: 80,
+              backgroundImage:
+                  AssetImage('assets/images/abdayprofile.jpg'),
+            ),
+            SizedBox(height: 15),
+            Text(
+              "Muhammad Abday Abdul Hafidz",
+              style: TextStyle(
+                fontFamily: 'Pixel',
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: Color(0xFFEAE6E5),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            Text(
+              "NIM: 1123150093 | KELAS: TI-23-SE-1",
+              style: TextStyle(
+                fontFamily: 'Pixel',
+                fontSize: 16,
+                color: Color(0xFFCCFB0F),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Keahlian",
+              style: TextStyle(
+                fontFamily: 'Pixel',
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: Color(0xFF473BFD),
               ),
             ),
+            SizedBox(height: 8),
+            Text(
+              "- Flutter Basic\n- JavaScript\n- HTML & CSS",
+              style: TextStyle(
+                fontFamily: 'Pixel',
+                fontSize: 16,
+                color: Color(0xFFEAE6E5),
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
+    ),
+  ),
+),
+
     );
   }
 }
