c3006a9021ac9f27c2113bca20a2f9a50fc8dd21
AryaPramudya898 <aryapramudya610@gmail.com>
2026-01-08 17:34:03 +0700

    feat: tanbah firebase messaging onbackground message

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
